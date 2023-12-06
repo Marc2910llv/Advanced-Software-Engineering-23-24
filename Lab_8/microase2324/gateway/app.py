587f525038bbc800cacddf2df277302eb5a86f10
Marc Llobera Villalonga <marclloberavillalonga@gmail.com>
2023-12-06 12:59:25 +0100

    Update

--- Lab_8/microase2324/gateway/app.py
+++ Lab_8/microase2324/gateway/app.py
@@ -1,35 +1,42 @@
 import requests
 
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, make_response
 from requests.exceptions import ConnectionError, HTTPError
 from urls import *
-import redis
+import os
+import subprocess # nosec
 
-ALLOWED_MATH_OPS = ['add', 'sub', 'mul', 'div', 'mod', 'crash']
-ALLOWED_STR_OPS = ['lower', 'upper', 'concat', 'crash']
+ALLOWED_MATH_OPS = ['add', 'sub', 'mul', 'div', 'mod', 'secure_random', 'log']
+ALLOWED_STR_OPS = ['lower', 'upper', 'concat', 'log']
 
 ids = {} #CAREFUL, THIS IS NOT FOR MULTIUSER AND MULTITHREADING, JUST FOR DEMO PURPOSES
 
 app = Flask(__name__, instance_relative_config=True)
 
-r = redis.Redis(host='db', port=6379, decode_responses=True)
 
 def create_app():
     return app
 
 @app.route('/math/<op>')
 def math(op):
-    a = request.args.get('a', type=float)
-    b = request.args.get('b', type=float)
     if op not in ALLOWED_MATH_OPS:
         return make_response('Invalid operation\n', 404)
     try:
+        a = request.args.get('a', type=float)
+        b = request.args.get('b', type=float)
+        lst = request.args.get('lst', type=str)
         M_URL=getMathURL()
-        if({op} == 'crash'):
+        if(op == 'crash'):
             URL = M_URL + f'/crash'
+        elif op == 'log':
+            URL = M_URL + f'/log'
+        elif lst is not None:
+            URL = M_URL + f'/{op}?lst={lst}'
         else:
+            a = request.args.get('a', type=float)
+            b = request.args.get('b', type=float)
             URL = M_URL + f'/{op}?a={a}&b={b}'
-        x = requests.get(URL)
+        x = requests.get(URL, timeout=1)
         x.raise_for_status()
         res = x.json()
         return res
@@ -40,7 +47,7 @@ def math(op):
                 URL = M_URL + f'/crash'
             else:
                 URL = M_URL + f'/{op}?a={a}&b={b}'
-            x = requests.get(URL)
+            x = requests.get(URL, timeout=1)
             x.raise_for_status()
             res = x.json()
         except ConnectionError:
@@ -64,17 +71,20 @@ def getMathURL():
 
 @app.route('/str/<op>')
 def string(op):
-    a = request.args.get('a', type=str)
-    b = request.args.get('b', type=str)
     if op not in ALLOWED_STR_OPS:
         return make_response('Invalid operation\n', 404)
     try:
         if op == 'lower' or op == 'upper':
-            x = requests.get(STRING_URL + f'/{op}?a={a}')
+           a = request.args.get('a', type=str)
+           x = requests.get(STRING_URL + f'/{op}?a={a}', timeout=1)
         elif op == 'crash':
-            x = requests.get(STRING_URL + f'/crash')
+            x = requests.get(STRING_URL + f'/crash', timeout=1)
+        elif op == 'log':
+            x = requests.get(STRING_URL + f'/log', timeout=1)
         else:
-            x = requests.get(STRING_URL + f'/{op}?a={a}&b={b}')
+            a = request.args.get('a', type=str)
+            b = request.args.get('b', type=str)
+            x = requests.get(STRING_URL + f'/{op}?a={a}&b={b}', timeout=1)
         x.raise_for_status()
         return x.json()
     except ConnectionError:
@@ -91,6 +101,30 @@ def log(op):
     else:
         return make_response('Invalid operation\n', 404)
 
+@app.route('/log/count/<URL>')
+def countLogs(URL):
+    try:
+        x = requests.get(LOG_URL + f'/countLogs/{URL}', timeout=1)
+        x.raise_for_status()
+        return x.text
+    except ConnectionError:
+        return make_response('Log service is down\n', 404)
+    except HTTPError:
+        return make_response('Invalid input\n', 400)
+
+@app.route('/ping/<URL>')
+def ping(URL):
+    try:
+        r = subprocess.run(f'ping -w 2 -c 3 {URL}', capture_output=True, shell=True)
+
+        if b"100% packet loss" in r.stdout:
+            return make_response('service is down', 404)
+        else:
+            return make_response(r.stdout, 200)
+    except:
+        return make_response('error', 500)
+
+
 def crashLog():
     try:
         x = requests.get(LOG_URL + f'/crash')
@@ -101,7 +135,7 @@ def crashLog():
     except HTTPError:
         return make_response('Invalid input\n', 400)
 
-def getLogService():
+def getLogs():
     try:
         x = requests.get(LOG_URL + f'/getLogs')
         x.raise_for_status()
@@ -110,15 +144,3 @@ def getLogService():
         return make_response('Log service is down\n', 404)
     except HTTPError:
         return make_response('Invalid input\n', 400)
-
-def getLogDB():
-    res = {}
-    for key in r.scan_iter(match="*"):
-        value = r.get(key)
-        res.update({key : value})
-    return make_response(jsonify(res), 200)
-
-
-
-def getLogs():
-    return getLogService(a,b,op,res,URL)
